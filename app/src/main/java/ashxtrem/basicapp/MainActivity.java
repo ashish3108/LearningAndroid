f345b962c5647f6f7c059b8bc569e797fcbd5eee
ashish3108 <ashu.trip31@gmail.com>
2017-09-30 00:48:39 +0530

    dice_roll_app

---
+++ app/src/main/java/ashxtrem/basicapp/MainActivity.java
@@ -0,0 +1,50 @@
+package ashxtrem.basicapp;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+import static android.util.Log.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = (Button) findViewById(R.id.rollButton);
+
+        final ImageView leftDice = (ImageView) findViewById(R.id.image_leftdice);
+        final ImageView rightDice = (ImageView) findViewById(R.id.image_rightdice);
+
+        final int[] DiceImage = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(6);
+
+                leftDice.setImageResource(DiceImage[number]);
+                number = randomNumberGenerator.nextInt(6);
+                rightDice.setImageResource(DiceImage[number]);
+            }
+        });
+    }
+
+}
